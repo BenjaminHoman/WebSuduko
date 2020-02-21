bc73fb0a8b1c7d4404ba22b094f4ced37f9bb039
Ben Homan <benjaminhoman10100@gmail.com>
2020-02-20 21:29:46 -0500

    added templates engine, view, and a basic grid

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const app = express();
 const port = 3001;
 
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.render('index', {title: 'Suduko', phrase: 'eat more'});
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
