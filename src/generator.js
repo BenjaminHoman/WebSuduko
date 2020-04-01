163cfd0bf890652423418f94ad218d37de7a872c
Ben Homan <benjaminhoman10100@gmail.com>
2020-03-31 20:37:47 -0400

    removed rules as own file. Generator extends solver

--- src/generator.js
+++ src/generator.js
@@ -1,16 +1,24 @@
 const solver = require('./solver');
 const constants = require('./constants');
+const shuffle = require('shuffle-array');
 
-class SudukoGenerator extends solver.SudukoSolver {
-	constructor(){
-
+exports.SudukoGenerator = class extends solver.SudukoSolver {
+	constructor() {
+		const BLANK = [[0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0],
+					 [0,0,0,0,0,0,0,0,0]];
+		super(BLANK, 1);
 	}
 
-	randomInt(max) {
-  		return Math.floor(Math.random() * Math.floor(max));
-	}
-
-	generate(){
-
+	*nextNumber() {
+		for (var n of shuffle([1,2,3,4,5,6,7,8,9])){
+			yield n;
+		}
 	}
 }
--- src/solver.js
+++ src/solver.js
@@ -1,5 +1,55 @@
-const rules = require('./rules');
 const constants = require('./constants');
+
+class Rules {
+	constructor(){}
+
+	/*
+	*	Determine if placing the current value on the grid is valid
+	*	@param {int} - x position
+	*	@param {int} - y position
+	* 	@param {int} - value to be tested
+	*	@return {boolean}
+	*/
+	isValid(grid, x, y, value){
+		if (!this.checkRow(grid, y, value) || 
+			!this.checkColumn(grid, x, value) || 
+			!this.checkBlock(grid, x, y, value)){
+			return false;
+		}
+		return true;
+	}
+
+	checkRow(grid, y, value){
+		for (let i = 0; i < constants.GRID_SIZE; i++){
+			if (grid[y][i] == value){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	checkColumn(grid, x, value){
+		for (let i = 0; i < constants.GRID_SIZE; i++){
+			if (grid[i][x] == value){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	checkBlock(grid, x, y, value){
+		let x0 = Math.floor(x/constants.SUB_SIZE)*constants.SUB_SIZE;
+		let y0 = Math.floor(y/constants.SUB_SIZE)*constants.SUB_SIZE;
+		for (let blockY = 0; blockY < constants.SUB_SIZE; blockY++){
+			for (let blockX = 0; blockX < constants.SUB_SIZE; blockX++){
+				if (grid[y0+blockY][x0+blockX] == value){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
 
 exports.SudukoSolver = class {
 
@@ -11,6 +61,7 @@ exports.SudukoSolver = class {
 		this.solutionCount = 0;
 		this.grid = grid;
 		this.solutions = [];
+		this.rules = new Rules();
 		this.solve();
 	}
 
@@ -26,8 +77,8 @@ exports.SudukoSolver = class {
 		for (let y = 0; y < constants.GRID_SIZE; y++){
 			for (let x = 0; x < constants.GRID_SIZE; x++){
 				if (this.grid[y][x] == constants.BLANK){
-					for (let i = 1; i <= constants.MAX_NUMBERS; i++){
-						if (rules.isValid(this.grid, x, y, i)){
+					for (var i of this.nextNumber()){
+						if (this.rules.isValid(this.grid, x, y, i)){
 							this.grid[y][x] = i;
 							this.solve();
 							this.grid[y][x] = constants.BLANK;
@@ -39,6 +90,12 @@ exports.SudukoSolver = class {
 		}
 		this.solutionCount++;
 		this.solutions.push(this.snapshot(this.grid));
+	}
+
+	*nextNumber(){
+		for (var i = 1; i <= constants.MAX_NUMBERS; i++){
+			yield i;
+		}
 	}
 
 	/*
